4302fca13710b93f3f50cfdeac5e5b66c3531c4c
Gonzalo <100429091@alumnos.uc3m.es>
2021-03-05 17:29:29 +0100

    Añadir codigo base

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# G80.T08.EG3
+# Guided Exercise 3
+
+TXX.GYY.GE3
+
+Authors:
+
+<author 1>, <email author 1>, user name in github
+
+<author 2>, <email author 2>, user name in github
---
+++ src/unittest/python/test_request_access_code_tests.py
@@ -0,0 +1,10 @@
+import unittest
+from secure_all import AccessManager
+
+class MyTestCase(unittest.TestCase):
+    def test_something( self ):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
