24b61c0b5f9cca77ed698e5d56718415497632f3
Gonzalo <100429091@alumnos.uc3m.es>
2021-04-03 20:38:23 +0200

    Segundo test implementado

--- src/unittest/python/test_request_access_code_tests.py
+++ src/unittest/python/test_request_access_code_tests.py
@@ -1,13 +1,20 @@
 import unittest
-from secure_all import AccessManager, AccessRequest
+from secure_all import AccessManager, AccessRequest, AccessManagementException
 
 
 class MyTestCase(unittest.TestCase):
     def test_request_access_code_dni_valido(self):
-        req = AccessRequest("41694463V", "Jose Lopez", "Guest", "jllopez@inf.uc3m.es", 5)
+        datosPersona = ("41694463V", "Jose Lopez", "Guest", "jllopez@inf.uc3m.es", 5)
+        req = AccessRequest(*datosPersona)
         codigo_esperado = req.access_code
-        codigo = AccessManager().request_access_code("41694463V", "Jose Lopez", "Guest", "jllopez@inf.uc3m.es", 5)
+        codigo = AccessManager().request_access_code(*datosPersona)
         self.assertEqual(codigo, codigo_esperado)
+
+    def test_request_access_code_dni_solo_numeros(self):
+        datosPersona = ("41694463", "Jose Lopez", "Guest", "jllopez@inf.uc3m.es", 5)
+        with self.assertRaises(AccessManagementException) as res:
+            AccessManager().request_access_code(*datosPersona)
+        self.assertEqual(res.exception.message, "DNI no válido")
 
 
 if __name__ == '__main__':
